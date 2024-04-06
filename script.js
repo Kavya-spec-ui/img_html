01efe9cb0b6001bea1a0eaef1d5188dc9acf588f
Kavya <kavyarajputkr12@gmail.com>
2024-04-06 20:23:13 +0530

    Add review changes

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 function toggleNavbar() {
   var menu = document.querySelector('.navbar-menu');
+  var divToHide = document.querySelector('.side'); // Select the first div to hide
+  var secondDivToHide = document.querySelector('.right_side_footer'); // Select the second div to hide
+
   if (window.innerWidth <= 768) {
       menu.classList.toggle('show');
+      divToHide.style.display = 'none'; // Hide the first div
+      secondDivToHide.style.display = 'none'; // Hide the second div
   } else {
       menu.classList.remove('show');
+      divToHide.style.display = 'block'; // Show the first div
+      secondDivToHide.style.display = 'block'; // Show the second div
   }
 }
 
@@ -12,7 +19,15 @@ document.querySelector('.navbar-toggler').addEventListener('click', toggleNavbar
 window.addEventListener('resize', function() {
   if (window.innerWidth > 768) {
       var menu = document.querySelector('.navbar-menu');
+      var divToHide = document.querySelector('.side'); // Select the first div to hide
+      var secondDivToHide = document.querySelector('.right_side_footer'); // Select the second div to hide
       menu.classList.remove('show');
+      divToHide.style.display = 'block'; // Show the first div
+      secondDivToHide.style.display = 'block'; // Show the second div
+  } else {
+      var divToHide = document.querySelector('.side'); // Select the first div to hide
+      var secondDivToHide = document.querySelector('.right_side_footer'); // Select the second div to hide
+      divToHide.style.display = 'none'; // Hide the first div
+      secondDivToHide.style.display = 'none'; // Hide the second div
   }
 });
-
